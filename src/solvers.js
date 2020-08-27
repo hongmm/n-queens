ea79d9949e640c66fca88b05a9eca99c99594d5f
Matt Hong <matthew.m.hong@gmail.com>
2020-08-26 20:03:08 -0700

    partially completed findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -13,15 +13,40 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+//We will make a recursive function that shrinks the board by 1 dimension after each recursion
+// smallest case === smallest board
+// row,col position for the rook/queen
+// for loop of n cases, everytime you recurse, you pass the location of the first node down
+// so we need to save the location(s) where a piece has been added
+// first case === n = 4 -> next case is === n = 3
+// we can keep track of rows and cols we have already been in -> recurse -> we would still operate on n = 4 but we would check and see if the pointer is on these rows/cols
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]; // has to be a matrix
+  var bannedRows = [];
+  var bannedCols = [];
+
+  for (var i = 0; i < n; i++) { // rows
+    for (var j = 0; j < n; j++) { // cols
+      if (!bannedRows.includes(i) && !bannedCols.includes(j)) {
+        //solution.push([i,j]);
+        solution[i][j] = 1;
+        bannedRows.push(i);
+        bannedCols.push(j);
+      }
+    }
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
-
+//
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
